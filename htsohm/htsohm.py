257da2495df6be166564f7bf3f3d071519d73352
A Reino Kaija <a.r.kaija@gmail.com>
2017-03-26 18:08:10 -0400

    added methoded for querying all parent_ids in current generation; mutation strengths are now calculated for each parent in the previous generation at the end of their children's generation before selecting parents for the following generation

--- htsohm/htsohm.py
+++ htsohm/htsohm.py
@@ -254,6 +254,7 @@ def calculate_percent_children_in_bin(run_id, generation, bin):
         self (class): row in material table.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     Returns:
         Fraction of children in the same bin as parent (self).
     """
